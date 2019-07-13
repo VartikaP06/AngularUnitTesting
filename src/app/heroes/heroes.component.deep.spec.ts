24b61c1d3513d4a7ccbaa9bfa0833e766c007f9f
Vartika06 <magicalvartika06@gmail.com>
2019-07-13 23:00:38 +0530

    Testing the Routing Link

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -100,4 +100,18 @@ describe('HeoresComponent (deep tests)', () => {
     const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent;
     expect(heroText).toContain(name);
   });
+
+  it('should have the correct route for the first hero', () => {
+    mockHeroesService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+
+    const routerLink = heroComponents[0]
+      .query(By.directive(RouterLinkDirectiveStub))
+      .injector.get(RouterLinkDirectiveStub);
+
+    heroComponents[0].query(By.css('a')).triggerEventHandler('click', null);
+
+    expect(routerLink.navigatedTo).toBe('/detail/1');
+  });
 });
